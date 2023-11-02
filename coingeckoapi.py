b3cd36b88d2cd2ce5e1afb284c5fbdb258f2e90f
MattHum <m.humpeler@hotmail.com>
2023-11-02 21:44:39 +0100

    update

--- coingeckoapi.py
+++ coingeckoapi.py
@@ -14,14 +14,16 @@ import csv
 url = "https://api.coingecko.com/api/v3/simple/price?ids="
 from creds import apikey
 ak = apikey
+dict_http = {'&vs_currencies=':'eur', '&include_market_cap=':'false', '&include_24hr_vol=':'false', '&include_24hr_change=':'false', '&include_last_updated_at=':'false', '&precision=':'false', '&x_cg_demo_api_key=':ak}
+print(dict_http)
 my_list = ['bitcoin', 'bitcoin-cash', 'zcash', 'ethereum', 'litecoin', 'monero', 'dash', 'cardano', 'ripple', 'neo', 'eos', 'stellar', 'crypterium', 'polkadot', 'solana', 'dydx', 'matic-network', 'algorand', 'cosmos']
 #polygon => matic-network
 separator =','
 payload = separator.join(my_list)
 newstring = url + payload + "&vs_currencies=" + "eur" + "&include_market_cap=" + "false" + "&include_24hr_vol=" + "false" + "&include_24hr_change=" + "false" + "&include_last_updated_at=" + "false" + "&precision=" + "false" + "&x_cg_demo_api_key=" + ak
-response = requests.get(newstring) #call api
-API_data = response.json() #save json replay in dict
-print(response.status_code) # check if connection went ok = 200
+#response = requests.get(newstring) #call api
+#API_data = response.json() #save json replay in dict
+#print(response.status_code) # check if connection went ok = 200
 
 #save data from nested dict to regular dict
 API_data_cleen = {}
--- json-converter.py
+++ json-converter.py
@@ -1,10 +1,16 @@
-#import requests
+import requests
 #import json
 #import csv
 
+url = "https://api.coingecko.com/api/v3/simple/price?ids="
 from creds import apikey
 ak = apikey
-print(ak)
 
-newstring = "&vs_currencies=" + "eur" + "&include_market_cap=" + "false" + "&include_24hr_vol=" + "false" + "&include_24hr_change=" + "false" + "&include_last_updated_at=" + "false" + "&precision=" + "false" + "&x_cg_demo_api_key=" + ak
-print(newstring)
+dict_http = {'vs_currencies=':'eur', 'include_market_cap=':'false', 'include_24hr_vol=':'false', 'include_24hr_change=':'false', 'include_last_updated_at=':'false', 'precision=':'false', 'x_cg_demo_api_key=':ak}
+#req = requests.Request('GET', url , params=dict_http)
+r = requests.get(url, params=dict_http)
+#prepared = req.prepare() # Get a PreparedRequest object
+print (r.url)
+
+my_list = ['bitcoin', 'bitcoin-cash', 'zcash', 'ethereum', 'litecoin', 'monero', 'dash', 'cardano', 'ripple', 'neo', 'eos', 'stellar', 'crypterium', 'polkadot', 'solana', 'dydx', 'matic-network', 'algorand', 'cosmos']
+#polygon => matic-network
